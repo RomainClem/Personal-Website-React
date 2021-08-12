37976a88be94775d7f7e88812693e101b4131907
Romain Clemencon <romain.clemencon@mycit.ie>
2021-08-12 14:07:42 +0100

    Quick fix on the row + cols grid in Skill.

--- client/src/Components/Home/skills.js
+++ client/src/Components/Home/skills.js
@@ -15,36 +15,36 @@ class Skills extends Component {
             <div className="blue-bg">
                 <div className="wrapper">
                     <div className="px-4 py-5 text-center">
-                        <div className="row row-cols-3 g-4">
-                            <div className="col-md-12 align-self-center">
+                        <div className="row g-4">
+                            <div className="col-md-12">
                                 <h2 className="display-5 fw-bold">Skills</h2>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <FontAwesomeIcon className="fa-skills mx-lg-auto" icon={faJava} size="4x"/>
+                                <FontAwesomeIcon className="fa-skills " icon={faJava} size="4x"/>
                                 <h2>Java</h2>
                                 <p>Maven, JavaFX (MVC), JPA, JUnit, JDBC</p>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <FontAwesomeIcon className="fa-skills mx-lg-auto" icon={faDatabase} size="4x"/>
+                                <FontAwesomeIcon className="fa-skills " icon={faDatabase} size="4x"/>
                                 <h2>Databases</h2>
                                 <p>MongoDB, SQLite, MySQL, Neo4j (Graph)</p>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <FontAwesomeIcon className="fa-skills mx-lg-auto" icon={faJsSquare} size="4x"/>
+                                <FontAwesomeIcon className="fa-skills " icon={faJsSquare} size="4x"/>
                                 <h2>JavaScript</h2>
                                 <p>Node.js, Express, React</p>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <FontAwesomeIcon className="fa-skills mx-lg-auto" icon={faPython} size="4x"/>
+                                <FontAwesomeIcon className="fa-skills " icon={faPython} size="4x"/>
                                 <h2>Python</h2>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <FontAwesomeIcon className="fa-skills mx-lg-auto" icon={faHtml5} size="4x"/>
+                                <FontAwesomeIcon className="fa-skills " icon={faHtml5} size="4x"/>
                                 <h2>WebDev</h2>
                                 <p>HTML5, CSS, Bootstrap, Stripe</p>
                             </div>
                             <div className="col col-12 col-sm-4">
-                                <img src={CLogo} alt="C programing language Logo" />
+                                <img src={CLogo} className="fa-skills mx-lg-auto" alt="C programing language Logo" />
                                 <h2>C Programing</h2>
                             </div>
                         </div>
