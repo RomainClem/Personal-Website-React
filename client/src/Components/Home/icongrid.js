fdd7acee29507e119adccad552a923af26afb38c
Romain Clemencon <romain.clemencon@mycit.ie>
2021-09-07 13:08:42 +0100

    Removed text below contacts and added a CV button.

--- client/src/Components/Home/icongrid.js
+++ client/src/Components/Home/icongrid.js
@@ -13,29 +13,26 @@ class Icongrid extends Component {
                     <div className="row px-4 py-5 text-center">
                         <div className="icons col">
                             <a rel="noopener noreferrer"  target="_blank" className="btn px-0" href="https://www.linkedin.com/in/romainclemencon/">
-                                <FontAwesomeIcon className="fa-icon pb-1" icon={faLinkedin}/>
-                                <p className="mb-0 icon-link rounded">Linkedin</p>
+                                <FontAwesomeIcon className="fa-icon" icon={faLinkedin}/>
                             </a>
                         </div>
                         <div className="icons col">
                             <a rel="noopener noreferrer"  target="_blank" className="btn px-0" href="https://github.com/RomainClem">
-                                <FontAwesomeIcon className="fa-icon pb-1" icon={faGithub}/>
-                                <p className="mb-0 icon-link rounded">GitHub</p>
+                                <FontAwesomeIcon className="fa-icon" icon={faGithub}/>
                             </a>
                         </div>
                         <div className="icons col">
                             <a rel="noopener noreferrer"  target="_blank" className="btn px-0" href="mailto:romain.clemencon@mycit.ie">
-                                <FontAwesomeIcon className="fa-icon pb-1" icon={faEnvelope}/>
-                                <p className="mb-0 icon-link rounded">Email</p>
+                                <FontAwesomeIcon className="fa-icon" icon={faEnvelope}/>
                             </a>
                         </div>
-                        <div className="icons col">
+                        <div className="icons col d-flex align-items-center justify-content-center">
                             <Link to={CV} className="btn px-0" rel="noopener noreferrer"  target="_blank" >
-                                <FontAwesomeIcon className="fa-icon pb-1" icon={faFilePdf}/>
-                                <p className="mb-0 icon-link rounded">CV</p>
+                                <div className="mx-auto icon-link d-flex align-items-center justify-content-center">
+                                    <p className="mb-0 ">CV</p>
+                                </div>
                             </Link>
                         </div>
-
                 </div>
             </div>
         )
