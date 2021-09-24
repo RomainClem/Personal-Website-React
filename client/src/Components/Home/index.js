edfaba7506b51bb430a1f89ea0635a99da96e582
Romain Clemencon <romain.clemencon@mycit.ie>
2021-09-24 16:15:18 +0100

    Updated packages version.

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -3,6 +3,11 @@ import IconGrid from "./icongrid";
 import Profile from "./profile";
 import Skills from "./skills";
 
+/*
+TODO:
+      - Issue at bottom of home page when the screen width is greater than 1920px and footer isn't pushed to the bottom
+ */
+
 class Home extends Component {
     render() {
         return (
