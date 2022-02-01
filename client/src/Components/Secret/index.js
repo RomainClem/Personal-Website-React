62dcb322777e61abc9ed869ab64b7e9818a6d148
Romain Clemencon <romain.clemencon@mycit.ie>
2022-02-01 10:47:57 +0000

    test file added

--- client/src/Components/Secret/index.js
+++ client/src/Components/Secret/index.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import floppa from '../../Assets/images/Floppa.gif'
 import rat from '../../Assets/images/rat.gif'
-
+import { Link } from 'react-router-dom'
+import nessus from "../../Assets/NessusAgent-10.0.1-amzn.x86_64.rpm"
 
 class Secret extends Component {
     render() {
@@ -17,7 +18,9 @@ class Secret extends Component {
                        allowFullScreen/>
                 </div>
                 <div className="col-md-4 d-flex justify-content-center p-0">
-                    <img src={rat} className="flex img-fluid" alt="Monkey typing on typewriter" loading="lazy"/>
+                    <Link to={nessus} className="btn px-0" rel="noopener noreferrer"  target="_blank" >
+                        <img src={rat} className="flex img-fluid" alt="Monkey typing on typewriter" loading="lazy"/>
+                    </Link>
                 </div>
 
             </div>
