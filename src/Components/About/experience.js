483f855b1a0c919b5c6a4e8d90e7501deb615b28
Romain <romain.clemencon@mycit.ie>
2022-05-27 18:46:56 +0200

    Updated exprience to match CV.

--- src/Components/About/experience.js
+++ src/Components/About/experience.js
@@ -3,13 +3,58 @@ const Experience = () => {
     return (
         <div className="wrapper py-5">
             <div className="col-md">
+                
+            <div className="row flex-lg-row align-items-center">
+            <div className="bio col-lg-9 mx-auto">
+            <div className="blue-bg">
+                <h2 className="m-0 ps-1 text-white">
+                    Professional Experience
+                </h2>
+            </div>
+            </div>
+            <div className="col-lg-3"></div>
+            </div>
+            
+            <div className="row flex-lg-row align-items-center">
+                    <div className="bio col-lg-9 mx-auto">
+                        <div className="orange-bg-transparent mt-4">
+                            <h4 className="m-0 ps-1 text-white">
+                                Trellix
+                            </h4>
+                        </div>
+                        <div className="blue-bg-light-transparent mt-2">
+                            <h5 className="m-0 ps-1 text-white fst-italic">
+                                Intern Software Developer
+                            </h5>
+                        </div>
+                        <div className="fs-5 pt-2">
+                            <ul>
+                                <li className="about-list">Migrated internally hosted tools to the <b>cloud with AWS</b>.</li>
+                                <li className="about-list">Worked within the team in charge of <b>Cloud technologies</b> and another team in charge of the
+                                 infrastructure of <b>deployment automation for build processes</b>.</li>
+                                <li className="about-list"><b>Learned new technologies</b> for my tasks such as AWS (EC2, RDS, SES, CF), Jenkins, and Jira.</li>
+                                <li className="about-list"><b>Proposed solutions</b> by applying the skills and knowledge I acquired as a student.</li>
+                                <li className="about-list">Participated in all stages of the <b>Agile software development cycle</b>.</li>
+                                <li className="about-list"><b>Led</b> my project, and was responsible for its <b>success</b>.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="orange-bg-transparent mt-2">
+                            <h4 className="m-0 ps-1 font-monospace text-white text-center">
+                                Jan 2022 – Present
+                            </h4>
+                        </div>
+                        <div className="blue-bg-light-transparent mt-2">
+                            <h4 className="m-0 ps-1 font-monospace text-white text-center">
+                                Cork, Ireland
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="row flex-lg-row align-items-center">
                     <div className="bio col-lg-9 mx-auto">
-                        <div className="blue-bg">
-                            <h2 className="m-0 ps-1 text-white">
-                                Professional Experience
-                            </h2>
-                        </div>
                         <div className="orange-bg-transparent mt-4">
                             <h4 className="m-0 ps-1 text-white">
                                 Stripe via TELUS International Ireland
@@ -35,7 +80,7 @@ const Experience = () => {
                     <div className="col-lg-3">
                         <div className="orange-bg-transparent mt-2">
                             <h4 className="m-0 ps-1 font-monospace text-white text-center">
-                                Jul 2018 – Present
+                                Jul 2018 – Dec 2021
                             </h4>
                         </div>
                         <div className="blue-bg-light-transparent mt-2">
@@ -45,6 +90,8 @@ const Experience = () => {
                         </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     )
