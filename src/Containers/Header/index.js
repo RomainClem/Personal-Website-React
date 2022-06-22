66460e7b5775d320a98015703f6168b6f55bee86
Romain Clemencon <romain.clemencon@mycit.ie>
2022-06-22 11:09:11 +0100

    Changed some css for mobile phones and reduced navbar size.

--- src/Containers/Header/index.js
+++ src/Containers/Header/index.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     return (
         <nav className="bg-dark fixed-top">
             <div className="wrapper">
-                <div className="d-flex flex-wrap justify-content-center py-3">
+                <div className="d-flex flex-wrap justify-content-center pb-2">
                     <Route>
                         <Link to="/" className="box d-flex align-items-center mb-3 mb-lg-0 me-md-auto text-decoration-none text-white name">
                             <span className="fs-3 fw-bold">Romain Clemencon</span>
